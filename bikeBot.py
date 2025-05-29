3a382f1176cdcc6ffed7790267b7d08e552644f0
hdjsbwjjwb <64488449+hdjsbwjjwb@users.noreply.github.com>
2025-05-29 23:52:13 +0300

    Update bikeBot.py

--- bikeBot.py
+++ bikeBot.py
@@ -672,5 +672,8 @@ async def send_daily_report():
     )
     await bot.send_message(ADMIN_ID, text)
 
+async def main():
+    await dp.start_polling(bot)
+    
 if __name__ == "__main__":
     asyncio.run(main())
