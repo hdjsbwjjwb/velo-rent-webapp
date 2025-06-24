d65b097a17cdb3b87e3890cb85ce9e5098b37704
hdjsbwjjwb <64488449+hdjsbwjjwb@users.noreply.github.com>
2025-06-24 21:52:15 +0200

    Update script.js

---
+++ miniapp/script.js
@@ -0,0 +1,12 @@
+document.getElementById('location1').onclick = function() {
+    alert('Описание для Места 1');
+};
+
+document.getElementById('location2').onclick = function() {
+    alert('Описание для Места 2');
+};
+
+document.getElementById('location3').onclick = function() {
+    alert('Описание для Места 3');
+};
+
