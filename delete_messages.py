6b2672b5cfd3f842c11baa4c2884d3bcbb91de89
hdjsbwjjwb <64488449+hdjsbwjjwb@users.noreply.github.com>
2025-06-29 21:46:44 +0200

    Update delete_messages.py

--- delete_messages.py
+++ delete_messages.py
@@ -1,22 +1,24 @@
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import ParseMode
+from dotenv import load_dotenv
 import os
 
-# Загружаем токен бота
-TOKEN = os.getenv("BOT_TOKEN")
+# Загружаем токен из .env
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')  # Новый токен
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+# Удаление всех сообщений бота в чате
 @dp.message_handler()
 async def handle_message(message: types.Message):
-    # Если сообщение нарушает правила, удаляем его
-    if "Тройничок" in message.text.lower():  # Например, ищем слово "порно"
-        try:
-            # Удаляем сообщение
+    try:
+        # Если сообщение от бота, удаляем
+        if message.from_user.is_bot:
             await message.delete()
-            # Отправляем ответ пользователю, что сообщение удалено
-            await message.answer("Это сообщение нарушает правила и было удалено.", reply_markup=None)
-        except Exception as e:
-            print(f"Ошибка при удалении сообщения: {e}")
+            print(f"Удалено сообщение с ID {message.message_id}")
+    except Exception as e:
+        print(f"Ошибка при удалении сообщения: {e}")
 
 # Запуск бота
 if __name__ == "__main__":
